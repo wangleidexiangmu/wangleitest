16a72c519b878e398b3a95e8ba2b83c944347b8b
wanglei <1109073586@qq.com>
2019-04-09 17:59:16 +0800

    weixin

--- laravel56/app/Http/Controllers/Weixin/WeixinController.php
+++ laravel56/app/Http/Controllers/Weixin/WeixinController.php
@@ -10,4 +10,21 @@ class WeixinController extends Controller
     public function valid(){
         echo $_GET['echostr'];
     }
+    public function wxEvent(){
+        $content=file_get_contents("php://input");
+        $time=date('Y-m-d H:i:s');
+        $str=$time.$content."\n";
+        file_put_contents("logs/wx_event.log",$str,FILE_APPEND);
+        echo "SUCCESS";
+    }
+    public function getAccessToken(){
+        $rul="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID')'&secret='.env('WX_SECRET')'";
+        $response=file_get_contents($rul);
+        $arr=json_decode($response,true);
+        //缓存
+        $key='wx_access_token';
+        Redis::set($key,$arr['access_token']);
+        Redis::expire($key,3600);
+        return $arr['access_token'];
+    }
 }
