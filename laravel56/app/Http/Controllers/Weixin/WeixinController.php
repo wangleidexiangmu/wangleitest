32ae5960fa34614b255646a8f3e33d47e75075ca
wanglei <1109073586@qq.com>
2019-04-09 08:55:12 +0000

    gai

--- laravel56/app/Http/Controllers/Weixin/WeixinController.php
+++ laravel56/app/Http/Controllers/Weixin/WeixinController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Weixin;
-
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,8 +10,22 @@ class WeixinController extends Controller
     public function valid(){
         echo $_GET['echostr'];
     }
-<<<<<<< HEAD
+    public function wxEvent(){
+        $content=file_get_contents("php://input");
+        $time=date('Y-m-d H:i:s');
+        $str=$time.$content."\n";
+        file_put_contents("logs/wx_event.log",$str,FILE_APPEND);
+        echo "SUCCESS";
+    }
+    public function getAccessToken(){
+        $rul="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID')'&secret='.env('WX_SECRET')'";
+        $response=file_get_contents($rul);
+        $arr=json_decode($response,true);
+        //缓存
+        $key='wx_access_token';
+        Redis::set($key,$arr['access_token']);
+        Redis::expire($key,3600);
+        return $arr['access_token'];
+    }
 }
-=======
-}
->>>>>>> 0e624d51403376940265f1010a096b6bf5729d22
+
