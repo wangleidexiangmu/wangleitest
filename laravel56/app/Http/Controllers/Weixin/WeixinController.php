1cf0bbf4f616a3cbc2ffdcbc212f645a827208a1
wanglei <1109073586@qq.com>
2019-04-10 10:08:15 +0000

    weixin

--- laravel56/app/Http/Controllers/Weixin/WeixinController.php
+++ laravel56/app/Http/Controllers/Weixin/WeixinController.php
@@ -19,22 +19,40 @@ class WeixinController extends Controller
         echo "SUCCESS";
     }
     public function getAccessToken(){
-        $rul='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_SECRET').'';
-        //var_dump($rul);exit;
-        $response=file_get_contents($rul);
-        //var_dump($response);exit;
-        $arr=json_decode($response,true);
-     //  var_dump($arr);exit;
-        //缓存
+        //判断是否有缓存
         $key='wx_access_token';
-        if($arr){
-            echo 1;
+        $token=redis::get($key);
+        if($token) {
+
         }else{
+            $rul='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_SECRET').'';
+            //var_dump($rul);exit;
+            $response=file_get_contents($rul);
+            //var_dump($response);exit;
+            $arr=json_decode($response,true);
+            //  var_dump($arr);exit;
+            //缓存
+
             Redis::set($key,$arr['access_token']);
             Redis::expire($key,3600);
             return $arr['access_token'];
+
         }
+        return $token;
 
-
+    }
+    //测试
+    public function test()
+    {
+        $access_token = $this->getAccessToken();
+        echo 'token : '. $access_token;echo '</br>';
+    }
+    //获取微信用户信息
+    public function getUserInfo($openid)
+    {
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->getAccessToken().'&openid='.$openid.'&lang=zh_CN';
+        $data = file_get_contents($url);
+        $u = json_decode($data,true);
+        return $u;
     }
 }
