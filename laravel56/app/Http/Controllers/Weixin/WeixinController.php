a1f7b2ed23d23b020e4bc29223482bd522ecc035
wanglei <1109073586@qq.com>
2019-04-11 03:58:31 +0000

    菜单

--- laravel56/app/Http/Controllers/Weixin/WeixinController.php
+++ laravel56/app/Http/Controllers/Weixin/WeixinController.php
@@ -83,15 +83,36 @@ class WeixinController extends Controller
     }
     public function card(){
         $res='https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->getAccessToken().'';
-        $arrInfo = '"button"=>array(
-            array(
-                "type"=>"click",
-                "name"=>"今日歌曲",
-                "key"=>"V1001_TODAY_MUSIC"
-            ),
-        )';
-        $arr=json_encode($arrInfo);
-        $data=file_get_contents($res,'POST',$arr);
+        $arrInfo ='{
+            "button":[
+     {
+         "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+          "name":"菜单",
+           "sub_button":[
+           {
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+                "type":"miniprogram",
+                 "name":"wxa",
+                 "url":"http://mp.weixin.qq.com",
+                 "appid":"wx286b93c14bbf93aa",
+                 "pagepath":"pages/lunar/index"
+             },
+            {
+                "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
+ }';
+        $data=file_get_contents($res,$arrInfo);
 
         var_dump($data);
     }
